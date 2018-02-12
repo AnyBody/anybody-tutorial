ffab989b21f85c8fc944114406cabd828add6075
Morten Enemark Lund <melund@gmail.com>
2018-02-12 08:41:25 +0100

    Switch language back

--- conf.py
+++ conf.py
@@ -114,7 +114,7 @@ index_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = 'da'
+language = 'en'
 
 locale_dirs = ['locale/']
 
