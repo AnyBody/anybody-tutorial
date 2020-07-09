3c5cd79e99b1a127378d665cafafd661bca1d299
Morten Enemark Lund <melund@gmail.com>
2020-07-09 15:04:53 +0200

    Update version in tutorial

--- conf.py
+++ conf.py
@@ -143,14 +143,14 @@ pygments_style = "AnyScript"
 
 current_year = os.environ.get("YEAR", datetime.now().year)
 
-ams_version = os.environ.get("AMS_VERSION", "7.2.2")
+ams_version = os.environ.get("AMS_VERSION", "7.3.0")
 if not re.match("^\d\.\d\.\d", ams_version):
     raise ValueError("Wrong format for AMS version, environment variable")
 ams_version_short = ams_version.rpartition(".")[0]
 ams_version_x = ams_version_short + ".x"
 
 
-ammr_version = os.environ.get("AMMR_VERSION", "2.2.2")
+ammr_version = os.environ.get("AMMR_VERSION", "2.3.0")
 if not re.match("^\d\.\d\.\d", ammr_version):
     raise ValueError("Wrong format for AMMR version, environment variable")
 ammr_version_short = ammr_version.rpartition(".")[0]
